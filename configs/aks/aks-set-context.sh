94406df6769b637b40c00d7154488e20dab9df02
gustavo.kuno <gustavo.mkuno@gmail.com>
2024-05-22 06:47:59 -0300

    .

--- configs/aks/aks-set-context.sh
+++ configs/aks/aks-set-context.sh
@@ -4,7 +4,6 @@ set -e
 clusterName="aks-pick"
 resourceGroup="rg-dev-pick"
 
-
-az account set -s $$AZ_SUBSCRIPTION_ID
+az account set -s $AZ_SUBSCRIPTION_ID
 
 az aks get-credentials --admin --name $clusterName --resource-group $resourceGroup --overwrite-existing
---
+++ docs/examples-kubectl.md
@@ -0,0 +1,6 @@
+# Querys
+
+```bash
+# consulta por label
+kubectl --namespace monitoring get pods -l "release=kube-prometheus-stack"
+``` 
